77b6674e4efe5c310b266056aa2b721fd5bc6ae5
anabautzer <ac.bautzer@hotmail.com>
2026-03-16 18:19:28 -0300

    Delete src/Exercicio3.java

--- src/Exercicio3.java
+++
@@ -1,9 +0,0 @@
-public class Exercicio3 {
-    public static void main(String[] args) {
-        int valorA = 8;
-        int valorB = 0;
-        valorB = valorA;
-                System.out.println(valorA);
-                System.out.println(valorB);
-    }
-}
