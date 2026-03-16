73b10823d994fa5fbd5cf741f1b8be4ac39da70f
anabautzer <ac.bautzer@hotmail.com>
2026-03-16 18:19:36 -0300

    Delete src/Exercicio2.java

--- src/Exercicio2.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio2 {
-    public static void main(String[] args) {
-    int valorX = 5;
-            System.out.println(valorX);
-            valorX=20;
-                System.out.println(valorX);
-    }
-}
