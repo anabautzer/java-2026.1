14629dc14d8824a78961c2d5281c0d67d8e35b48
anabautzer <ac.bautzer@hotmail.com>
2026-03-16 18:42:31 -0300

    Add files via upload

---
+++ exemplo-1/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {  // método main que é o método chamado para executar o código
+    // {} define o escopo do método, ou seja, onde inicia e onde termina
+    IO.println("Hello World"); // elemento para mostrar conteúdo na tela
+    // imprimi os dados posicionando o cursor na próxima linha
+    // () após cada instrução
+    IO.print(""); // imprimi os dados posicionando o cursor na mesma linha
+}
